3696fc9e4d7ed2b8dde1cd81ef3ed400a20b1b75
SimonJiang <simonj142857@gmail.com>
2016-09-20 19:39:06 -0400

    NPC without dialogue bug fixed

--- GameDev1/FlixelTut/source/NPC.hx
+++ GameDev1/FlixelTut/source/NPC.hx
@@ -36,23 +36,28 @@ class NPC extends FlxSprite
 	private var _dialogue_cleared:Array<String>;
 	private var _task:Array<String>;	//For stating the requirements
 
-	private var _choice_good:Int32;
-	private var _choice_neutral:Int32;
-	private var _choice_evil:Int32;
+	private var _choice_good:Int;
+	private var _choice_neutral:Int;
+	private var _choice_evil:Int;
 
 	private var _cleared:Bool;
 	private var _talking:Bool;
 
-	private var npc_sprite = new FlxSprite();
 	private var sprite_path:String;
+	private var sprite_width:Int;
+	private var sprite_height:Int;
 
 	var rot:Float = 0;
 
 	//Constructor
-	public function new()
+	public function new(?X:Float=0, ?Y:Float=0, W:Int, H:Int)
 	{
+		super(X, Y);
 		_cleared = false;
 		_talking = false;
+		sprite_width = W;
+		sprite_height = H;
+
 	}
 
 	public function set_sprite_path(p:String):Void
@@ -60,28 +65,25 @@ class NPC extends FlxSprite
 		sprite_path = p;
 	}
 
+	
 	public function load_sprite():Bool
 	{
-		if(npc_sprite = loadGraphic(sprite_path))
-		{
-			add(npc_sprite);
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		loadGraphic(sprite_path, false, sprite_width, sprite_height);
+		return true;
 	}
+	
 
-	public function set_value(choice: Int32, val: Int32): Void
+	public function set_value(choice: Int, val: Int): Void
 	{
-		switch(choice):
+		switch(choice)
+		{
 			case 0:
 				_choice_good = val;
 			case 1:
 				_choice_neutral = val;
 			case 2:
 				_choice_evil = val;
+		}
 	}
 
 	public function status(): Bool
@@ -89,7 +91,7 @@ class NPC extends FlxSprite
 		return _cleared;
 	}
 
-	public function make_choice(choice: Int32): Int32
+	public function make_choice(choice: Int): Int
 	{
 		switch(choice)
 		{
@@ -103,6 +105,7 @@ class NPC extends FlxSprite
 				//evil choice
 				return _choice_evil;
 		}
+		return 0;
 	}
 
 
@@ -115,7 +118,8 @@ class NPC extends FlxSprite
 	{
 		if(_cleared)
 		{
-			//show _dialogue_cleared text
+			//show _dialogue_cleared text. Show words by words if possible. 
+
 		}
 		else
 		{
--- GameDev1/FlixelTut/source/PlayState.hx
+++ GameDev1/FlixelTut/source/PlayState.hx
@@ -11,11 +11,73 @@ class PlayState extends FlxState
 {
 	var _player:Player;
 	
+	var _npc_1:NPC;
+	var _npc_2:NPC;
+	var _npc_3:NPC;
+	var _npc_4:NPC;
+	var _npc_5:NPC;
+	var _npc_6:NPC;
+	var _npc_7:NPC;
+	var _npc_8:NPC;
+	var _npc_9:NPC;
+	
 	override public function create():Void
 	{
-		_player = new Player(20,20);
+		_player = new Player(20, 20);
 		add(_player);
 		FlxG.camera.follow(_player);
+
+		/* NPC Creation Section */
+		_npc_1 = new NPC(50, 260, 30, 70);
+		_npc_1.set_sprite_path("assets/images/npc 1.png");
+		_npc_1.load_sprite();
+		add(_npc_1);
+
+		_npc_2 = new NPC(130, 120, 30, 70);
+		_npc_2.set_sprite_path("assets/images/npc 2.png");
+		_npc_2.load_sprite();
+		add(_npc_2);
+
+		_npc_3 = new NPC(200, 50, 30, 70);
+		_npc_3.set_sprite_path("assets/images/npc 3.png");
+		_npc_3.load_sprite();
+		add(_npc_3);
+
+		_npc_4 = new NPC(20, 0, 30, 70);
+		_npc_4.set_sprite_path("assets/images/npc 4.png");
+		_npc_4.load_sprite();
+		add(_npc_4);
+
+		_npc_5 = new NPC(300, 150, 30, 70);
+		_npc_5.set_sprite_path("assets/images/npc 5.png");
+		_npc_5.load_sprite();
+		add(_npc_5);
+
+		_npc_6 = new NPC(80, 250, 30, 70);
+		_npc_6.set_sprite_path("assets/images/npc 6.png");
+		_npc_6.load_sprite();
+		add(_npc_6);
+
+		_npc_7 = new NPC(160, 333, 30, 70);
+		_npc_7.set_sprite_path("assets/images/npc 7.png");
+		_npc_7.load_sprite();
+		add(_npc_7);
+
+		_npc_8 = new NPC(500, 300, 30, 70);
+		_npc_8.set_sprite_path("assets/images/npc 8.png");
+		_npc_8.load_sprite();
+		add(_npc_8);
+
+		_npc_9 = new NPC(500, 360, 30, 70);
+		_npc_9.set_sprite_path("assets/images/npc 9.png");
+		_npc_9.load_sprite();
+		add(_npc_9);
+
+
+
+
+
+
 		super.create();
 	}
 
